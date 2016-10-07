41dda947e5062f1e5173fe86bd7cdf070be1d030
Ravi Pandya <ravpandy@cisco.com>
2016-10-07 13:08:17 -0700

    kafka python code

---
+++ python/kafka-producer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import threading, logging, time
+
+from kafka import KafkaConsumer, KafkaProducer
+
+
+class Producer(threading.Thread):
+    daemon = True
+
+    def run(self):
+        producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+        while True:
+            producer.send('my-topic', b"test")
+            producer.send('my-topic', b"\xc2Hola, mundo!")
+            time.sleep(1)
+
+def main():
+    threads = [
+        Producer(),
+    ]
+
+    for t in threads:
+        t.start()
+
+    time.sleep(10)
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s',
+        level=logging.INFO
+        )
+    main()
